bf12239ff4d4b3946215317425bd09f4440e5ce7
Nick Bhat <nick_bhat@berkeley.edu>
2020-10-13 12:42:40 -0700

    Add metrics with passing tests.

---
+++ mogwai/utils/metrics.py
@@ -0,0 +1,75 @@
+from typing import List
+
+import numpy as np
+import torch
+from scipy.stats import entropy
+
+
+def precision_at_cutoff(
+    pred: torch.Tensor,
+    meas: torch.Tensor,
+    cutoff: int = 1,
+    thresh: float = 0.01,
+    superdiag: int = 6,
+):
+    """Computes precision for top L/10, L/9, ... L"""
+    # Subset everything above superdiag
+    eval_idx = np.triu_indices_from(meas, superdiag)
+    pred_, meas_ = pred[eval_idx], meas[eval_idx]
+
+    # Sort by model confidence
+    sort_idx = pred_.argsort(descending=True)
+
+    # Extract top predictions and calculate
+    len_cutoff = (len(meas) / torch.tensor(cutoff)).int()
+    preds = meas_[sort_idx][:len_cutoff]
+
+    num_positives = (meas_[sort_idx][:len_cutoff] > thresh).sum().float()
+    num_preds = len(preds)
+    precision = num_positives / num_preds
+
+    return precision
+
+
+def contact_auc(
+    pred: torch.Tensor,
+    meas: torch.Tensor,
+    thresh: float = 0.01,
+    superdiag: int = 6,
+    cutoff_range: List[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+):
+    # Nick: This does not agree with normal AUPR, but instead computes
+    # precision for top L/10, L/9, ... then averages them together.
+    # Compared to normal precision this puts more weight on a small number
+    # of high confidence true positives.
+
+    # True aupr could be computed via
+    #
+    # from sklearn.metrics import average_precision_score
+    #
+    # eval_idx = np.triu_indices_from(meas, superdiag)
+    # pred_, meas_ = pred[eval_idx], meas[eval_idx]
+    # aupr = average_precision_score(meas_ > thresh, pred_)
+
+    binned_precisions = [
+        precision_at_cutoff(pred, meas, c, thresh, superdiag) for c in cutoff_range
+    ]
+    return torch.stack(binned_precisions, 0).mean()
+
+
+def get_len_stdev(msa):
+    lengths = []
+    for seq in msa:
+        num_gaps = len(np.where(seq == 20)[0])
+        len_unaligned = len(seq) - num_gaps
+        lengths.append(len_unaligned)
+    return np.std(lengths)
+
+
+def get_len_entropy(msa):
+    lengths = []
+    for seq in msa:
+        num_gaps = len(np.where(seq == 20)[0])
+        len_unaligned = len(seq) - num_gaps
+        lengths.append(len_unaligned)
+    return entropy(lengths)
---
+++ test_metrics.py
@@ -0,0 +1,65 @@
+import torch
+import unittest
+
+from mogwai.utils.metrics import contact_auc, precision_at_cutoff
+
+
+class TestPrecision(unittest.TestCase):
+    def setUp(self):
+        self.pred = torch.FloatTensor(
+            [
+                [1e-3, 1e-2, 0.8],
+                [1e-2, 1e-4, 0.3],
+                [0.8, 0.3, 1e-10],
+            ]
+        )
+        self.meas = torch.IntTensor([[0, 1, 1], [1, 0, 0], [1, 1, 0]])
+
+    def test_precision_cutoffs(self):
+        p_at_1 = precision_at_cutoff(self.pred, self.meas, cutoff=1, superdiag=0)
+        p_at_2 = precision_at_cutoff(self.pred, self.meas, cutoff=2, superdiag=0)
+        p_at_3 = precision_at_cutoff(self.pred, self.meas, cutoff=3, superdiag=0)
+
+        self.assertEqual(p_at_1, 2.0 / 3)
+        self.assertEqual(p_at_2, 1.0)
+        self.assertEqual(p_at_3, 1.0)
+
+    def test_superdiag(self):
+        superdiag_0 = precision_at_cutoff(self.pred, self.meas, cutoff=1, superdiag=0)
+        superdiag_1 = precision_at_cutoff(self.pred, self.meas, cutoff=1, superdiag=1)
+        superdiag_2 = precision_at_cutoff(self.pred, self.meas, cutoff=1, superdiag=2)
+        superdiag_3 = precision_at_cutoff(self.pred, self.meas, cutoff=1, superdiag=3)
+        self.assertEqual(superdiag_0, 2.0 / 3)
+        self.assertEqual(superdiag_1, 2.0 / 3)
+        self.assertEqual(superdiag_2, 1.0)
+        self.assertTrue(superdiag_3.isnan())
+
+
+class TestAUC(unittest.TestCase):
+    def setUp(self):
+        self.pred = torch.FloatTensor(
+            [
+                [1e-3, 1e-2, 0.8],
+                [1e-2, 1e-4, 0.3],
+                [0.8, 0.3, 1e-10],
+            ]
+        )
+        self.meas = torch.IntTensor([[0, 1, 1], [1, 0, 0], [1, 1, 0]])
+
+    def test_range(self):
+        auc = contact_auc(self.pred, self.meas, superdiag=0, cutoff_range=[1, 2, 3])
+        self.assertEqual(auc, 8.0 / 9)
+
+    def test_superdiag_range(self):
+        auc_superdiag_1 = contact_auc(
+            self.pred, self.meas, superdiag=1, cutoff_range=[1, 2, 3]
+        )
+        auc_superdiag_2 = contact_auc(
+            self.pred, self.meas, superdiag=2, cutoff_range=[1, 2, 3]
+        )
+        self.assertEqual(auc_superdiag_1, 8.0 / 9)
+        self.assertEqual(auc_superdiag_2, 1.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
