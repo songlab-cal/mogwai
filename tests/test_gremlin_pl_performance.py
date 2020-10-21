7bacc3ce6ea036dfab82f6a69a539314e267430c
Nick Bhat <nick_bhat@berkeley.edu>
2020-10-21 14:40:34 -0700

    Switch to A3M example.

--- tests/test_gremlin_pl_performance.py
+++ tests/test_gremlin_pl_performance.py
@@ -3,30 +3,32 @@ import unittest
 
 import pytorch_lightning as pl
 
-from mogwai.data_loading import NPZ_MSADataModule
+from mogwai.data_loading import A3M_MSADataModule, parse_cf
 from mogwai.models import GremlinPseudolikelihood
 
 
 class TestGremlinPLPerformance(unittest.TestCase):
     def setUp(self):
-        npz_path = "data/test/3er7_1_A.npz"
-        self.dm = NPZ_MSADataModule(npz_path, batch_size=4096)
+        npz_path = "data/test/4rb6Y/4rb6Y.i90c75.a3m"
+        self.dm = A3M_MSADataModule(npz_path, batch_size=4096)
         self.dm.setup()
+
+        true_contacts = parse_cf("data/test/4rb6Y/4rb6Y.cf")
 
         n, l, _ = self.dm.dims
         self.model = GremlinPseudolikelihood(
-            n, l, self.dm.msa_counts, true_contacts=self.dm.true_contacts
+            n, l, self.dm.msa_counts, true_contacts=torch.tensor(true_contacts)
         )
         self.trainer = pl.Trainer(
-            min_steps=200,
-            max_steps=200,
+            min_steps=50,
+            max_steps=50,
             gpus=1,
         )
 
     def test_training_performance(self):
         self.trainer.fit(self.model, self.dm)
         final_auc_apc = self.model.get_auc(do_apc=True)
-        self.assertGreaterEqual(final_auc_apc, 0.74)
+        self.assertGreaterEqual(final_auc_apc, 0.91)
 
 
 if __name__ == "__main__":
