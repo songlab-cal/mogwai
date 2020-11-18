187c7fc92718730351f64a9e6c03a8bbef7e00e0
Roshan Rao <rmrao@berkeley.edu>
2020-11-18 08:04:36 -0800

    support stockholm format + also saving just the contact predictions

--- mogwai/vocab.py
+++ mogwai/vocab.py
@@ -40,8 +40,11 @@ class _FastaVocab:
     def convert_indices_to_tokens(self, indices: Sequence[int]) -> List[str]:
         return [self.ALPHABET[i] for i in indices]
 
-    def convert_tokens_to_indices(self, tokens: Sequence[str]) -> List[int]:
-        return [self.A2N.get(token, 20) for token in tokens]
+    def convert_tokens_to_indices(self, tokens: Sequence[str], skip_unknown: bool = False) -> List[int]:
+        if skip_unknown:
+            return [self.A2N[token] for token in tokens if token in self.A2N]
+        else:
+            return [self.A2N.get(token, 20) for token in tokens]
 
     def tokenize(self, sequence: str) -> List[int]:
         return self.convert_tokens_to_indices(list(sequence))
